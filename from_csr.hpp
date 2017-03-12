966b8a99e769584d7460318b6de7302ae1a3d67e
Jeff Trull <edaskel@att.net>
2017-03-11 19:48:26 -0800

    Introduce a new CSR-to-CSR approach using standard algorithms

--- from_csr.hpp
+++ from_csr.hpp
@@ -1,6 +1,7 @@
 
 #include <vector>
 #include <map>
+#include <numeric>
 
 #include "nothing.hpp"
 
@@ -28,6 +29,56 @@ void run(std::vector<IndexT> const & A_rows, std::vector<IndexT> const & A_cols,
   do_nothing(B);
 }
 
+
+// create outer pointers my way, for comparison
+template<typename IndexT, typename NumericT>
+void
+stdalg_run(std::vector<IndexT> const & A_rows, std::vector<IndexT> const & A_cols, std::vector<NumericT> const & A_values,
+           std::vector<IndexT>       & B_rows, std::vector<IndexT>       & B_cols, std::vector<NumericT>       & B_values)
+{
+    std::size_t N = A_rows.size() - 1;
+
+    B_cols.resize(A_cols.size());
+    B_values.resize(A_values.size()); // note: initialization with zero not strictly required
+    B_rows = std::vector<IndexT>(A_rows.size(), 0);    // valid only for square matrices
+
+    // 1) count the number of entries in each column (soon to be row)
+    std::for_each(A_cols.begin(), A_cols.end(),
+                  [&B_rows](IndexT col) { ++B_rows[col+1]; });
+
+    // 2) perform an "exclusive sum" on the result
+    std::partial_sum(B_rows.begin(), B_rows.end(), B_rows.begin());
+
+    // 3) copy values (and move rows to columns)
+
+    for (IndexT col_in_A = 0; col_in_A < N; ++col_in_A) {
+        // this is the loop that should run in parallel
+        // use counting iterator
+
+        IndexT B_offset = B_rows[col_in_A]; // index of first unwritten element in this row
+
+        // next phase: an iterator returning row and value pairs for a given column
+        // write this custom with iterator_facade
+        // finally zip iterator and run copy?  But B_offsets would have to be zipped iterators
+
+        // look for this column in each row
+        for (IndexT row = 0; row < N; ++row) {
+            // take advantage of sorted column numbers to locate, if present
+            auto col_range = std::equal_range(A_cols.begin() + A_rows[row],
+                                              A_cols.begin() + A_rows[row+1],
+                                              col_in_A);
+            if (col_range.first != col_range.second) {
+                // non-empty range means we have found the target value
+                // Add this row (and the associated value) to B
+                IndexT nnz_index = std::distance(A_cols.begin(), col_range.first);
+                IndexT B_nnz_index = B_offset;
+                B_cols[B_nnz_index] = row;
+                B_values[B_nnz_index] = A_values[nnz_index];
+                B_offset++;
+            }
+        }
+    }
+}
 
 //
 // CSR to CSR
